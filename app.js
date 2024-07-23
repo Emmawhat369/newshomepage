273d81524f1cd253f774bbd00149792dc4fe2785
Emmawhat369 <emanamir019@gmail.com>
2024-07-23 22:47:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById('hiddenmenu').style.display = 'block';
+    document.getElementById('menubtn').style.display = 'none';
+}
+
+function closeMenu(){
+    document.getElementById('hiddenmenu').style.display = 'none';
+    document.getElementById('menubtn').style.display = 'block';
+}
